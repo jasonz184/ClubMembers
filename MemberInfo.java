1f03f18b42abada3aa775fb4d45004793fd7e62e
jasonz184 <jasonz184@nycstudents.net>
2025-03-18 15:40:47 +0000

    Wrote all methods and added all test cases

---
+++ MemberInfo.java
@@ -0,0 +1,36 @@
+public class MemberInfo 
+{
+    /**
+     * Constructs a MemberInfo object for the club member with name name,
+     * graduation year gradYear, and standing hasGoodStanding.
+     */
+    private String name;
+    private int gradYear;
+    private boolean hasGoodStanding;
+    public MemberInfo(String name, int gradYear, boolean hasGoodStanding) 
+    {
+        this.name = name;
+        this.gradYear = gradYear;
+        this.hasGoodStanding = hasGoodStanding;
+    }
+
+    public String toString()
+    {
+        String s = "\nName: " + name + "\n";
+        s += "Year: " + gradYear + "\n";
+        return s + "Good Standing: " + hasGoodStanding +"\n";
+    }
+    /** Returns the graduation year of the club member. */
+    public int getGradYear() 
+    {
+        return gradYear;
+    }
+
+    /** Returns true if the member is in good standing and false otherwise. */
+    public boolean inGoodStanding() 
+    {
+        return hasGoodStanding;
+    }
+    // There may be instance variables, constructors, and methods that are not
+    // shown.
+}
